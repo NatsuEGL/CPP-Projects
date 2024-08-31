648cc26dda23fdd3cef1d5e297b628e08b7b9908
NatsuEGL <54336789+NatsuEGL@users.noreply.github.com>
2024-08-31 07:50:06 +0100

    Add files via upload
    adding second ex that talked about array's and hundling them by templates

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,22 @@
 /*   By: akaabi <akaabi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 11:27:46 by akaabi            #+#    #+#             */
-/*   Updated: 2024/08/28 11:27:47 by akaabi           ###   ########.fr       */
+/*   Updated: 2024/08/31 07:39:32 by akaabi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_HPP
+#define ITER_HPP
+
+
+#include <iostream>
+
+template <typename T, typename F>
+void iter(T* array, size_t length, F func) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
+
+
+#endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,38 @@
 /*   By: akaabi <akaabi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 11:27:49 by akaabi            #+#    #+#             */
-/*   Updated: 2024/08/28 11:27:50 by akaabi           ###   ########.fr       */
+/*   Updated: 2024/08/31 07:49:09 by akaabi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+// integer
+void printInt(int x) {
+    std::cout << x << " ";
+}
+
+// string
+void printString(const std::string& str) {
+    std::cout << str << " ";
+}
+
+int main() {
+    // Test with an array of integers
+    int intArray[] = {1, 2, 3, 4, 5};
+    size_t intArrayLength = sizeof(intArray) / sizeof(intArray[0]);
+    
+    std::cout << "Integer array: ";
+    ::iter(intArray, intArrayLength, printInt);
+    std::cout << std::endl;
+
+
+    std::string t[] = {"hello", "amine", "Natsu" , "salam"};
+    int arraysize = sizeof(t) / sizeof(t[0]);
+
+    std::cout << "String array: ";
+    ::iter(t, arraysize, printString);
+    std::cout << std::endl;
+    
+    return 0;
+}
